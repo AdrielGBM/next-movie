7743438e8789572d93c34d0c73985ce7fdec4671
Adriel Gonzalo Barrientos Mercado <122940027+Adrielgbm@users.noreply.github.com>
2025-03-08 18:30:38 -0300

    feat: add custom hook useResponsiveWidth for tracking window width

---
+++ src/hooks/useResponsiveWidth.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+function useResponsiveWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return width;
+}
+
+export default useResponsiveWidth;
