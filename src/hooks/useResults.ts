d849f665bb82cf87141224ca7d749259a43189ba
Adriel Gonzalo Barrientos Mercado <122940027+Adrielgbm@users.noreply.github.com>
2025-03-15 18:44:42 -0300

    feat: enhance useResults hook to fill empty results with translations from Spanish and English

--- src/hooks/useResults.ts
+++ src/hooks/useResults.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import get from "../services/get";
-import { Results } from "../types/media";
+import { Results, Movie, Series } from "../types/media";
 
 function useResults<Data>(path: string) {
   const [loading, setLoading] = useState<boolean>(true);
@@ -11,9 +11,47 @@ function useResults<Data>(path: string) {
     async function getData() {
       try {
         setLoading(true);
-        const result = await get<Results<Data>>(
+        const mxResults = await get<Results<Data>>(
           "/3" + path + (path.includes("?") ? "&" : "?") + "language=es-MX"
         );
+        const result = mxResults;
+
+        const esResults = await get<Results<Data>>(
+          "/3" + path + (path.includes("?") ? "&" : "?") + "language=es-ES"
+        );
+        (result as Results<Movie> | Results<Series>).results.forEach(
+          (m, index) => {
+            if (m.overview === "") {
+              if (
+                (esResults as Results<Movie> | Results<Series>).results[index]
+                  .overview !== ""
+              ) {
+                (result as Results<Movie> | Results<Series>).results[index] = (
+                  esResults as Results<Movie> | Results<Series>
+                ).results[index];
+              }
+            }
+          }
+        );
+
+        const enResults = await get<Results<Data>>(
+          "/3" + path + (path.includes("?") ? "&" : "?") + "language=en-US"
+        );
+        (result as Results<Movie> | Results<Series>).results.forEach(
+          (m, index) => {
+            if (m.overview === "") {
+              if (
+                (enResults as Results<Movie> | Results<Series>).results[index]
+                  .overview !== ""
+              ) {
+                (result as Results<Movie> | Results<Series>).results[index] = (
+                  enResults as Results<Movie> | Results<Series>
+                ).results[index];
+              }
+            }
+          }
+        );
+
         setData(result);
       } catch (error) {
         setError(error instanceof Error ? error.message : "Error desconocido");
