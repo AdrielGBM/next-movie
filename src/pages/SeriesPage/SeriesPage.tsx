c10a2804fb264fbdabec5a4e02714a897566106f
Adriel Gonzalo Barrientos Mercado <122940027+Adrielgbm@users.noreply.github.com>
2025-03-22 18:46:44 -0300

    feat: enhance SeriesPage with additional search parameters for improved filtering

--- src/pages/SeriesPage/SeriesPage.tsx
+++ src/pages/SeriesPage/SeriesPage.tsx
@@ -10,20 +10,55 @@ import DiscoverTemplate from "../../components/templates/DiscoverTemplate/Discov
 function SeriesPage() {
   const [searchParams, setSearchParams] = useSearchParams();
   const defaultParams = useMemo(
-    () => ({
-      page: ["page", "1"],
-      vote_count_gte: ["vote_count.gte", "100"],
-    }),
+    () =>
+      ({
+        page: ["page", "1"],
+        sort_by: ["sort_by", "popularity.desc"],
+        vote_average_gte: ["vote_average.gte", "0"],
+        vote_average_lte: ["vote_average.lte", "100"],
+        vote_count_gte: ["vote_count.gte", "100"],
+        vote_count_lte: ["vote_count.lte", "0"],
+        with_genres: ["with_genres", ""],
+      } as Record<string, [string, string]>),
     []
   );
   const [params, setParams] = useState({
     page: searchParams.get("page") ?? defaultParams.page[1],
+    sort_by: searchParams.get("sort_by") ?? defaultParams.sort_by[1],
+    vote_average_gte:
+      searchParams.get("vote_average.gte") ?? defaultParams.vote_average_gte[1],
+    vote_average_lte:
+      searchParams.get("vote_average.lte") ?? defaultParams.vote_average_lte[1],
     vote_count_gte:
       searchParams.get("vote_count.gte") ?? defaultParams.vote_count_gte[1],
+    vote_count_lte:
+      searchParams.get("vote_count.lte") ?? defaultParams.vote_count_lte[1],
+    with_genres:
+      searchParams.get("with_genres") ?? defaultParams.with_genres[1],
   });
 
   const path = useMemo(() => {
-    return `/discover/tv?page=${params.page}&vote_count.gte=${params.vote_count_gte}`;
+    const queryParams = [
+      `page=${params.page}`,
+      params.sort_by !== "popularity.desc" ? `sort_by=${params.sort_by}` : null,
+      params.vote_average_gte !== "0"
+        ? `vote_average.gte=${params.vote_average_gte}`
+        : null,
+      params.vote_average_lte !== "0"
+        ? `vote_average.lte=${params.vote_average_lte}`
+        : null,
+      params.vote_count_gte !== "0"
+        ? `vote_count.gte=${params.vote_count_gte}`
+        : null,
+      params.vote_count_lte !== "0"
+        ? `vote_count.lte=${params.vote_count_lte}`
+        : null,
+      params.with_genres ? `with_genres=${params.with_genres}` : null,
+    ]
+      .filter(Boolean)
+      .join("&");
+
+    return `/discover/tv?${queryParams}`;
   }, [params]);
 
   const { data: genresData } = useGenres();
@@ -32,8 +67,19 @@ function SeriesPage() {
   useEffect(() => {
     setParams({
       page: searchParams.get("page") ?? defaultParams.page[1],
+      sort_by: searchParams.get("sort_by") ?? defaultParams.sort_by[1],
+      vote_average_gte:
+        searchParams.get("vote_average.gte") ??
+        defaultParams.vote_average_gte[1],
+      vote_average_lte:
+        searchParams.get("vote_average.lte") ??
+        defaultParams.vote_average_lte[1],
       vote_count_gte:
         searchParams.get("vote_count.gte") ?? defaultParams.vote_count_gte[1],
+      vote_count_lte:
+        searchParams.get("vote_count.lte") ?? defaultParams.vote_count_lte[1],
+      with_genres:
+        searchParams.get("with_genres") ?? defaultParams.with_genres[1],
     });
   }, [searchParams, defaultParams]);
 
@@ -41,6 +87,7 @@ function SeriesPage() {
     <>
       <DiscoverTemplate
         title="Series"
+        searchParams={searchParams}
         setSearchParams={setSearchParams}
         genresData={genresData ? (genresData.genres as Genre[]) : undefined}
         mediaData={{
