ac2e70713f90239b0aedc5cf1f6fcbda13d46402
Adriel Gonzalo Barrientos Mercado <122940027+Adrielgbm@users.noreply.github.com>
2025-03-25 15:55:37 -0300

    feat: integrate useImages hook in SeriesDetailsPage for fetching images alongside details

--- src/pages/SeriesDetailsPage/SeriesDetailsPage.tsx
+++ src/pages/SeriesDetailsPage/SeriesDetailsPage.tsx
@@ -1,22 +1,37 @@
 import { useParams } from "react-router";
 import useDetails from "../../hooks/useDetails";
+import useImages from "../../hooks/useImages";
+import { SeriesDetails } from "../../types/media";
 import "./SeriesDetailsPage.scss";
 
 import DetailsTemplate from "../../components/templates/DetailsTemplate/DetailsTemplate";
-import { SeriesDetails } from "../../types/media";
 
 function SeriesDetailsPage() {
   const { tvId } = useParams();
 
-  const { loading, data, error } = useDetails(`/tv/${tvId ?? ""}`);
+  const {
+    loading: detailsLoading,
+    data: DetailsData,
+    error: DetailsError,
+  } = useDetails(`/tv/${tvId ?? ""}`);
+  const {
+    loading: imagesLoading,
+    data: imagesData,
+    error: imagesError,
+  } = useImages(`/tv/${tvId ?? ""}`);
 
   return (
     <>
       <DetailsTemplate
         series={{
-          loading: loading,
-          data: data as SeriesDetails,
-          error: error,
+          loading: detailsLoading,
+          data: DetailsData as SeriesDetails,
+          error: DetailsError,
+        }}
+        images={{
+          loading: imagesLoading,
+          data: imagesData,
+          error: imagesError,
         }}
       />
     </>
